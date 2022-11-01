cec4153cd6af93f53768259cad6c820a3c7b0940
unknown <usrikanthvarma1996@gmail.com>
2022-11-01 23:28:36 +0530

    2 commit

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ const App=()=> {
     <div >
       <container>
      <Accordion>
-  <h1>Hello, world!</h1>
+  <h1>Hello, world! pora <span style={{color:'red'}}>ananth</span>erri pooka</h1>
   <p>
     This is a simple hero unit, a simple jumbotron-style component for calling
     extra attention to featured content or information.
   </p>
+  
   <p>
     <Button variant="primary">Learn more</Button>
   </p>
